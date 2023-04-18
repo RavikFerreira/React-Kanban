5f2a575bb0eb83311a28d3730798267f1a7631c4
RavikFerreira <ravikferreira.developer@gmail.com>
2023-04-18 16:30:54 -0300

    PropTypes e useStates(hooks) implementation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+import React from "react";
+import "./App.css";
 
-import './App.css';
-
-import NavBar from './components/NavBar/NavBar';
+import NavBar from "./components/NavBar/NavBar";
+import TaskList from "./components/TaskList/TaskList";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+
+      <div className="container">
+        <TaskList title="Pendente" />
+        <TaskList title="Fazendo" />
+        <TaskList title="Completo" />
+        
+        </div>
+
+      
+
     </div>
+      
+    
   );
 }
 
